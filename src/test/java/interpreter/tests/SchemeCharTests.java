a3631c1c4efbbd64ef2d7ca209015a3a5ae93abc
a-bastrykina <alena0212@gmail.com>
2020-12-24 06:30:27 +0700

    test for identifier eval

--- src/test/java/interpreter/tests/SchemeCharTests.java
+++ src/test/java/interpreter/tests/SchemeCharTests.java
@@ -2,6 +2,7 @@ package interpreter.tests;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
 import org.junit.Test;
 import ru.nsu.fit.lispmachine.machine.execution_context.ExecutionContext;
 import ru.nsu.fit.lispmachine.machine.interpreter.SchemeChar;
