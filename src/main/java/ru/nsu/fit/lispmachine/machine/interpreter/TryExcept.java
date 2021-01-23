00c4391e549434b142f82614391f584e7eb1fcc3
a-bastrykina <alena0212@gmail.com>
2021-01-24 01:35:30 +0700

    fix try-catch evaluation

--- src/main/java/ru/nsu/fit/lispmachine/machine/interpreter/TryExcept.java
+++ src/main/java/ru/nsu/fit/lispmachine/machine/interpreter/TryExcept.java
@@ -3,45 +3,44 @@ package ru.nsu.fit.lispmachine.machine.interpreter;
 import ru.nsu.fit.lispmachine.machine.execution_context.ExecutionContext;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class TryExcept implements Expression {
 
-    private final Expression body;
-    private final List<SchemeIdentifier> exceptionNames;
-    private final List<Expression> catchBodies;
+	private final Expression body;
+	private final Map<SchemeString, Expression> exceptionsAndCatches;
 
-    public TryExcept(Expression body, List<SchemeIdentifier> exceptionNames, List<Expression> catchBodies) {
-        this.body = body;
-        this.exceptionNames = exceptionNames;
-        this.catchBodies = catchBodies;
-    }
+	public TryExcept(Expression body, Map<SchemeString, Expression> exceptionsAndCatches) {
+		this.body = Objects.requireNonNull(body);
+		this.exceptionsAndCatches = Objects.requireNonNull(exceptionsAndCatches);
+	}
 
 	@Override
 	public Expression evaluate(ExecutionContext context) {
-        try {
-            return body.evaluate(context);
-        }
-        catch (Exception exc) {
-            for (int i = 0; i < exceptionNames.size(); i++) {
-                if (("class " + exceptionNames.get(i).toString()).equals(exc.getClass().toString())) {
-                    return catchBodies.get(i).evaluate(context);
-                }
-            }
-            throw new IllegalArgumentException(exc);
-        }
+		try {
+			return body.evaluate(context);
+		} catch (Exception exc) {
+			var excName = exc.getClass().getName();
+			var catchBody = exceptionsAndCatches.get(new SchemeString(excName));
+			if (catchBody != null) {
+				return catchBody.evaluate(context);
+			}
+			throw new IllegalArgumentException("Could find matching catch clause for " + exc.getClass().getName(), exc);
+		}
 	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TryExcept tryExcept = (TryExcept) o;
-        return Objects.equals(body, tryExcept.body) && Objects.equals(exceptionNames, tryExcept.exceptionNames) && Objects.equals(catchBodies, tryExcept.catchBodies);
-    }
+	@Override public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		TryExcept tryExcept = (TryExcept) o;
+		return body.equals(tryExcept.body) &&
+				exceptionsAndCatches.equals(tryExcept.exceptionsAndCatches);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(body, exceptionNames, catchBodies);
-    }
+	@Override public int hashCode() {
+		return Objects.hash(body, exceptionsAndCatches);
+	}
 }
--- src/test/java/ru/nsu/fit/lispmachine/interpreter/TryExceptTest.java
+++ src/test/java/ru/nsu/fit/lispmachine/interpreter/TryExceptTest.java
@@ -6,43 +6,44 @@ import ru.nsu.fit.lispmachine.machine.execution_context.SchemeContext;
 import ru.nsu.fit.lispmachine.machine.interpreter.*;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
-//todo : move to homemade tests when parser will parse (try (body) (catch "extype1" (catch-body1) "extype1" (catch-body1) ... )
 public class TryExceptTest {
 
-    @Test
-    public void testEvaluateNoException() {
-        var body = new Application(new SchemeIdentifier("+"), List.of(new SchemeNumber(6), new SchemeNumber(2)));
-        var prog = new TryExcept(body, List.of(new SchemeIdentifier("java.lang.DivisionByZero")), List.of(new SchemeNumber(12345)));
-        var executionContext = new SchemeContext();
-        var actualExpr = prog.evaluate(executionContext);
-        var expectedExpr = new SchemeNumber(8);
-        assertEquals(expectedExpr, actualExpr);
-    }
+	@Test
+	public void testEvaluateNoException() {
+		var body = new Application(new SchemeIdentifier("+"), List.of(new SchemeNumber(6), new SchemeNumber(2)));
+		var prog = new TryExcept(body,
+				Map.of(new SchemeString("java.lang.DivisionByZero"), new SchemeNumber(12345)));
+		var executionContext = new SchemeContext();
+		var actualExpr = prog.evaluate(executionContext);
+		var expectedExpr = new SchemeNumber(8);
+		assertEquals(expectedExpr, actualExpr);
+	}
 
-    @Test
-    public void testEvaluateWithException() {
-        var body = new Application(new SchemeIdentifier("/"), List.of(new SchemeNumber(0), new SchemeNumber(0)));
-        var catchExpression = new SchemeNumber(12345);
-        var prog = new TryExcept(body, List.of(new SchemeIdentifier("java.lang.ArithmeticException")),List.of(catchExpression));
-        var executionContext = new SchemeContext();
-        var actualExpr = prog.evaluate(executionContext);
-        assertEquals(catchExpression, actualExpr);
-    }
+	@Test
+	public void testEvaluateWithException() {
+		var body = new Application(new SchemeIdentifier("/"), List.of(new SchemeNumber(0), new SchemeNumber(0)));
+		var catchExpression = new SchemeNumber(12345);
+		var prog = new TryExcept(body, Map.of(new SchemeString("java.lang.ArithmeticException"), catchExpression));
+		var executionContext = new SchemeContext();
+		var actualExpr = prog.evaluate(executionContext);
+		assertEquals(catchExpression, actualExpr);
+	}
 
 
-    //todo: uncomment this
-//    @Test
-//    public void testEvaluateWithCustomException() {
-//        var catchExpression = new SchemeNumber(12345);
-//        var prog = new TryExcept(new Application(new SchemeIdentifier("throw"), List.of(new SchemeString("java.lang.IllegalArgumentException"), new SchemeString("look!"))),
-//                catchExpression);
-//        var executionContext = new SchemeContext();
-//        var actualExpr = prog.evaluate(executionContext);
-//        assertEquals(catchExpression, actualExpr);
-//    }
+	//todo: uncomment this
+	//    @Test
+	//    public void testEvaluateWithCustomException() {
+	//        var catchExpression = new SchemeNumber(12345);
+	//        var prog = new TryExcept(new Application(new SchemeIdentifier("throw"), List.of(new SchemeString("java.lang.IllegalArgumentException"), new SchemeString("look!"))),
+	//                catchExpression);
+	//        var executionContext = new SchemeContext();
+	//        var actualExpr = prog.evaluate(executionContext);
+	//        assertEquals(catchExpression, actualExpr);
+	//    }
 
 }
